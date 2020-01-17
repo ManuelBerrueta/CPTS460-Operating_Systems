c0fd7ad4ce551e74b80f18bff04f9d93060c4a11
Manuel Berrueta <manuelberrueta@gmail.com>
2020-01-16 21:33:46 -0800

    WIP on LAB1.2

--- LAB1.2/samples/bs.s
+++ LAB1.2/samples/bs.s
@@ -1,57 +1,48 @@
-!============================================================================
-        .globl _main,_prints,_color             ! IMPORT symbols
-        .globl _getc,_putc,_readfd,_error       ! EXPORT symbols
+        BOOTSEG =  0x9000        ! Boot block is loaded again to here.
+        SSP      =   8192        ! Stack pointer at SS+8KB
+	
+        .globl _main,_prints              ! IMPORT symbols
+        .globl _getc,_putc                ! EXPORT symbols
+
+	.globl _readfd,_setes,_inces,_error  
 	                                                
-        mov  ax,#0x9000     ! set ES to 0x9000
+        !-------------------------------------------------------
+        ! Only one SECTOR loaded at (0000,7C00). Get entire BLOCK in
+        !-------------------------------------------------------
+        mov  ax,#BOOTSEG    ! set ES to 0x9000
         mov  es,ax
         xor  bx,bx          ! clear BX = 0
 
-        
+        !---------------------------------------------------
+        !  read boot BLOCK to [0x9000,0]     
+        !---------------------------------------------------
         xor  dx,dx          ! drive 0, head 0
-        xor  cx,cx          ! cyl 0,   sector 0
+        xor  cx,cx          ! cyl 0    sector 0
         incb cl             ! cyl 0, sector 1
         mov  ax, #0x0202    ! READ 1 block
         int  0x13
 
-        jmpi    start,0x9000            ! CS=0x9000, IP=start
+        jmpi    start,BOOTSEG           ! CS=BOOTSEG, IP=start
 
 start:                    
         mov     ax,cs                   ! Set segment registers for CPU
         mov     ds,ax                   ! we know ES,CS=0x9000. Let DS=CS  
         mov     ss,ax                   ! SS = CS ===> all point at 0x9000
         mov     es,ax
-        mov     sp,#8192                ! SP = 8KB above SS=0x9000
+        mov     sp,#SSP                 ! SP = 8KB above SS=0x9000
 
         mov     ax,#0x0012              ! 640x480 color     
 	int     0x10 
 	
         call _main                      ! call main() in C
-  
+     
+        test ax, ax
+	je  _error 
+
         jmpi 0,0x1000
  
 
 !======================== I/O functions =================================
-       !---------------------------------------
-       ! readfd(cyl, head, sector, buf)
-       !        4     6     8      10
-       !---------------------------------------
-_readfd:                             
-        push  bp
-	mov   bp,sp            ! bp = stack frame pointer
-
-        movb  dl, #0x00        ! drive 0=FD0
-        movb  dh, 6[bp]        ! head
-        movb  cl, 8[bp]        ! sector
-        incb  cl               ! BIOS count sector from 1
-        movb  ch, 4[bp]        ! cyl
-        mov   bx, 10[bp]       ! BX=buf ==> memory addr=(ES,BX)
-        mov   ax, #0x0202      ! READ 2 sectors to (EX, BX)
-
-        int  0x13              ! call BIOS to read the block 
-        jb   _error            ! to error if CarryBit is on [read failed]
-
-        pop  bp                
-	ret
 		
         !---------------------------------------------
         !  char getc()   function: returns a char
@@ -70,12 +61,52 @@ _putc:
 	
         movb   al,4[bp]        ! get the char into aL
         movb   ah,#14          ! aH = 14
-!        movb   bl,#0x0D        ! bL = cyan color 
-        movb   bl, _color
+        movb   bl,#0x0D        ! bL = cyan color 
         int    0x10            ! call BIOS to display the char
 
         pop    bp
 	ret
+
+
+	
+
+       !---------------------------------------
+       ! readfd(cyl, head, sector, buf)
+       !         4     6     8     10
+       !---------------------------------------
+_readfd:                             
+        push  bp
+	mov   bp,sp            ! bp = stack frame pointer
+
+        movb  dl, #0x00        ! drive 0=FD0
+        movb  dh, 6[bp]        ! head
+        movb  cl, 8[bp]        ! sector
+        incb  cl               ! BIOS count sector from 1
+        movb  ch, 4[bp]        ! cyl
+        mov   bx, 10[bp]       ! BX=buf ==> memory addr=(ES,BX)
+        mov   ax, #0x0202      ! READ 2 sectors to (EX, BX)
+
+        int  0x13              ! call BIOS to read the block 
+        jb   _error            ! to error if CarryBit is on [read failed]
+
+        pop  bp                
+	ret
+
+        
+_setes:  push  bp
+	 mov   bp,sp
+
+         mov   ax,4[bp]        
+         mov   es,ax
+
+	 pop   bp
+	 ret
+	
+_inces:                         ! inces() inc ES by 0x40, or 1K
+         mov   ax,es
+         add   ax,#0x40
+         mov   es,ax
+         ret
 
         !------------------------------
         !       error & reboot
--- LAB1.2/t.c
+++ LAB1.2/t.c
@@ -30,6 +30,7 @@ typedef unsigned long u32;
 #define BLK 1024
 
 #include "ext2.h"
+
 typedef struct ext2_group_desc GD;
 typedef struct ext2_inode INODE;
 typedef struct ext2_dir_entry_2 DIR;
@@ -41,15 +42,31 @@ char buf1[BLK], buf2[BLK];
 int color = 0x0A;
 u8 ino;
 
+
 int prints(char *s)
 {
-    extern _putc(s);
+    while(*s){
+        putc(*s);
+        s++;
+    }
+    
+    return 1;
 }
+
 
 int gets(char *s)
 {
-    extern _getc(s);
+    /* While temp does not equal '\r'*/
+    while((*s = getc()) != 0x0D){
+        putc(*s);
+        s++;
+    }
+    s++;
+    *s = 0;
+
+    return 1;
 }
+
 
 int getblk(u16 blk, char *buf)
 {
@@ -57,9 +74,47 @@ int getblk(u16 blk, char *buf)
     readfd(blk / 18, ((blk) % 18) / 9, (((blk) % 18) % 9) << 1, buf);
 }
 
+
+u16 search(INODE *ip, char *fname)
+{
+    int i=0;
+
+    //prints("in search func");
+    for(i=0; i < 12; i++)
+    {
+        if ((u16)ip->i_block[i] == 0)
+        {
+            break;
+        }
+        getblk((u16)ip->i_block[i], buf2);
+        dp = (DIR *)buf2;
+
+        // BLK = Block Size = 1024
+        while((char *)dp < buf2 + BLK)
+        {
+            //strncpy(temp, dp->name, dp->name_len);
+            //temp[dp->name_len] = 0; //add null char to the end off the dp->name
+            dp->name[dp->name_len] = 0;
+
+            //TODO: strcmp to see if the given name exists
+            //if (strcmp(fname, temp) == 0)
+            if (strcmp(fname, dp->name) == 0)
+            {
+                prints("found ino for ");
+                prints(fname);
+                return (u16)dp->inode;
+            }
+            dp = (char *)dp + dp->rec_len;
+        }
+    }
+    //printf("**inode %s, not found in data blocks\n\n", fname);
+    return 0;
+}
+
+
 main()
 {
-    u16 i, iblk;
+    u16  i, ino, iblk;
     char c, temp[64];
 
     prints("TESTING\n");
@@ -68,15 +123,28 @@ main()
     getblk(2, buf1);
 
     // 1. WRITE YOUR CODE to get iblk = bg_inode_table block number
+    gp = (GD *)buf1;                    //Group Descriptor
+    iblk = (u16)gp->bg_inode_table;     //Start of inode table
     prints("inode_block=");
     putc(iblk + '0');
     prints("\n\r");
 
     // 2. WRITE YOUR CODE to get root inode
     prints("read inodes begin block to get root inode\n\r");
+    //Load the INODES from the  iblk into buf1
+    getblk(iblk, buf1);
+    ip = (INODE *)buf1; //Cast the start of buf1 is an INODE
+    ip++; //INODE #2 == root
+
 
     // 3. WRITE YOUR CODE to step through the data block of root inode
     prints("read data block of root DIR\n\r");
+    ino = search(ip, "boot");
+
+    
+    getblk(iblk + (ino/8), buf2);
+
+
 
     // 4. print file names in the root directory /
 }
